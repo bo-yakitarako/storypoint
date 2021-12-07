0290f2d4869f14d35484214d8cccf4b1335cbac9
bo-yakitarako <bo-yakitarako@users.noreply.github.com>
2021-12-07 19:38:27 +0900

    Gopherくんかわいいねえ

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,31 @@
 import React from 'react';
-import { ChakraProvider } from '@chakra-ui/react';
+import { Box, ChakraProvider, Flex, Image } from '@chakra-ui/react';
 import { NameDialog } from './NameDialog';
+import { Form } from './Form';
 
 const App: React.FC = () => {
   return (
     <ChakraProvider>
       <NameDialog />
+      <Flex
+        justifyContent="center"
+        alignItems="center"
+        width="100%"
+        minHeight="100vh"
+      >
+        <Box>
+          <Form />
+        </Box>
+      </Flex>
+      <Image
+        alt="かわいいかわいいGopherくんかわいいねえかわいいよ"
+        src="/images/gopher.png"
+        position="fixed"
+        bottom="0"
+        right="-200px"
+        width="600px"
+        zIndex="-1"
+      />
     </ChakraProvider>
   );
 };
